f32a34d8a193b61d5bc2347664e7e4a18dcc1c95
vijakar <svijakar07@gmail.com>
2024-05-26 06:48:01 +0530

    Navbar Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
